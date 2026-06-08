83c56ebfcdfeed5485eb9c309169810be9eed9fc
Aggiiie <agust.starck@gmail.com>
2026-06-08 18:18:19 +0200

    Basic Character Controller + Camera.

---
+++ scripts/player/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const ACCELERATION = 15.0
+const FRICTION = 12.0
+
+@export var sensitivity = 0.002
+
+@onready var camera: Camera3D = $Camera3D
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, -PI / 3, PI / 3)
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
+	var direction := (transform.basis * Vector3(input.x, 0, input.y)).normalized()
+
+	if direction:
+		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION * delta)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+		velocity.z = move_toward(velocity.z, 0, FRICTION * delta)
+
+	move_and_slide()
