c32af8e2a740694f315b78032c7d4d21ed4d7893
Mhiw <103310488+Mhiw@users.noreply.github.com>
2026-06-14 09:38:46 +0200

    First working version

--- scripts/camera/camera.gd
+++ scripts/camera/camera.gd
@@ -1,15 +1,29 @@
 extends Node3D
 
 @onready var picture_scene = preload("res://scenes/picture.tscn")
+@export var viewport: SubViewport
+@export var camera: Camera3D
+@export var camera_point: Node3D
 
 func take_picture() -> void:
+	await RenderingServer.frame_post_draw
+	
 	var instance = picture_scene.instantiate()
-	#var image = $Sprite3D.texture.get_image()
-	#instance.find_child("Sprite3D").texture = ImageTexture.create_from_image(image)
+	
+	var viewport_texture: ViewportTexture = viewport.get_texture()
+	#var image_texture: ImageTexture = ImageTexture.create_from_image(viewport_texture.get_image())
+	
+	instance.find_child("Sprite3D").texture = viewport_texture
+	
+	#image_texture.get_image().save_png("res://img.png")
+	
 	instance.position = Vector3(0, 10, 0)
-	print(get_tree().root.name)
 	get_tree().root.add_child(instance)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("take_picture"):
 		take_picture()
+	
+	camera.global_position = camera_point.global_position
+	#camera.position = Vector3(0, 10, 0)
+	#print(camera_point.global_position)
